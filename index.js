9384192388feee2087696b95765a0d9832df8298
Ahmed Mariam <157092587+mariamahmed224@users.noreply.github.com>
2024-06-06 09:17:30 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,92 @@
+import { test1, test2, test3, test4, nail2 } from "../assets";
+import { whatsapp, faceboook, instagram } from "../assets";
+
+
+
+
+
+export const testimony = [
+    {
+        image: test1,
+        title: 'felicia andrew',
+        content: 'they really met my needs. i loved the customer care alot'
+    },
+    {
+        image: test2,
+        title: 'joseph martin',
+        content: 'i loved the ambiance of the salon. the man that attended to me was really nice and good'
+    },
+    {
+        image: test3,
+        title: 'Brad ',
+        content: 'i loved my hair cut'
+    },
+    {
+        image: test4,
+        title: 'Zara Felix',
+        content: 'i would really come back because i loved it there'
+    },
+]
+
+export  const navLinks = [
+    {
+      id: "home",
+      title: "Home",
+    },
+
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "gallery",
+      title: "Gallery",
+    },
+    {
+      id: "services",
+      title: "Services",
+    },
+    {
+      id: "testimonial",
+      title: "Testimonial",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+
+  export const footerLink = [
+    {
+      img: faceboook,
+      href: "https://www.facebook.com/profile.php?id=100073011550116&mibextid=LQQJ4d",
+    },
+   
+    {
+      img: instagram,
+      href: "https://www.instagram.com/sun2528moon/",
+    },
+    {
+      img: whatsapp,
+      href: "https://wa.me/14136401794",
+    },
+  ];
+
+  export const galleryNails = [
+    {
+      id: "nail1",
+      img: nail2,
+    },
+    {
+      id: "nail1",
+      img: nail2,
+    },
+    {
+      id: "nail1",
+      img: nail2,
+    },
+    {
+      id: "nail1",
+      img: nail2,
+    },
+  ];
